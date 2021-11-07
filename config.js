89aec2a058cc5bd4eebd5cd246bcd3a6f001729e
nguyenthat98 <93864829+nguyenthat98@users.noreply.github.com>
2021-11-07 18:50:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ const CONFIG = {
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^I love duuu changg^^',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Đỗ Thị Thu Trang <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Mình rất yêu bạn <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Yêu chang nhiufuuuu nhiufuuuu <333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/dthutrang1510
 }
